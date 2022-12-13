ac11d87c61d43c96d3d2156393f1b5a24fb02fec
SmallUpdate <112966801+SmallUpdate@users.noreply.github.com>
2022-12-13 18:23:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,95 +1,17 @@
 $(function() {
 
-  $(".chapter_1").click(function(){
-    $('.chapter_1_lessons').toggleClass("lessons_show")
-    $('.chapter_1').toggleClass("btn_active")
+  $(".hamburger").click(function(){
+    $('.header_hidden').toggleClass("hidden_show")
+    $('.dark').toggleClass("dark_show")
   });
 
-  $(".chapter_2").click(function(){
-    $('.chapter_2_lessons').toggleClass("lessons_show")
-    $('.chapter_2').toggleClass("btn_active")
+  $(".header_close_box").click(function(){
+    $('.header_hidden').toggleClass("hidden_show")
+    $('.dark').toggleClass("dark_show")
   });
 
-  $(".chapter_3").click(function(){
-    $('.chapter_3_lessons').toggleClass("lessons_show")
-    $('.chapter_3').toggleClass("btn_active")
-  });
-
-  $(".chapter_4").click(function(){
-    $('.chapter_4_lessons').toggleClass("lessons_show")
-    $('.chapter_4').toggleClass("btn_active")
-  });
-});
-
-$(function() {
-  $("#first").click(function(){
-    $('#first').toggleClass("lesson_active")
-    $('#first_section').toggleClass("section_show")
-  });
-  
-  $("#second").click(function(){
-    $('#second').toggleClass("lesson_active")
-    $('#second_section').toggleClass("section_show")
-  });
-
-  $("#third").click(function(){
-    $('#third').toggleClass("lesson_active")
-    $('#third_section').toggleClass("section_show")
-  });
-  
-  $("#fourth").click(function(){
-    $('#fourth').toggleClass("lesson_active")
-    $('#fourth_section').toggleClass("section_show")
-  });
-  
-  $("#fifth").click(function(){
-    $('#fifth').toggleClass("lesson_active")
-    $('#fifth_section').toggleClass("section_show")
-  });
-  
-  $("#sixth").click(function(){
-    $('#sixth').toggleClass("lesson_active")
-    $('#sixth_section').toggleClass("section_show")
-  });
-
-  $("#seventh").click(function(){
-    $('#seventh').toggleClass("lesson_active")
-    $('#seventh_section').toggleClass("section_show")
-  });
-  
-  $("#eighth").click(function(){
-    $('#eighth').toggleClass("lesson_active")
-  });
-  
-  $("#ninth").click(function(){
-    $('#ninth').toggleClass("lesson_active")
-  });
-  
-  $("#tenth").click(function(){
-    $('#tenth').toggleClass("lesson_active")
-  });
-
-  $("#eleventh").click(function(){
-    $('#eleventh').toggleClass("lesson_active")
-  });
-  
-  $("#twelweth").click(function(){
-    $('#twelweth').toggleClass("lesson_active")
-  });
-  
-  $("#thirteenth").click(function(){
-    $('#thirteenth').toggleClass("lesson_active")
-  });
-  
-  $("#fourteenth").click(function(){
-    $('#fourteenth').toggleClass("lesson_active")
-  });
-
-  $("#fiveteenth").click(function(){
-    $('#fiveteenth').toggleClass("lesson_active")
-  });
-  
-  $("#sixteenth").click(function(){
-    $('#sixteenth').toggleClass("lesson_active")
+  $(".screen_button").click(function(){
+    $('.test').toggleClass("test_start")
+    $('.dark').toggleClass("dark_show")
   });
 });
